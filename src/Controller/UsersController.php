ee0d2d8903f63d581881d1159ed4310bc6716659
NikhithasriMasana <104629994+NikhithasriMasana@users.noreply.github.com>
2022-08-02 19:15:45 +0530

    initial commit

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -54,7 +54,7 @@ class UsersController extends AppController
         if ($this->request->is('post')) {
             $user = $this->Users->patchEntity($user, $this->request->getData());
             if ($this->Users->save($user)) {
-                $this->Flash->success(__('The user has been saved.'));
+                $this->Flash->success(__('The user has been saved at.'));
 
                 return $this->redirect(['action' => 'index']);
             }
